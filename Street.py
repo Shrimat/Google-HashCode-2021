255f5cc28834d7c0922b181a2d9dcc8e8e8d83b2
Shrimat Kapoor <shrimatkapoor@gmail.com>
2021-02-25 19:17:13 +0000

    Adds updating code for cars on street

--- Street.py
+++ Street.py
@@ -6,10 +6,14 @@ class Street:
         self.end_intersection = end_node
         self.travel_time = travel_time
         self.waiting_cars = []
-        self.travelling_cars = [] #list of tuples (car, start time)
+        self.travelling_cars = []  # list of tuples (car, start time)
 
-    def update_travelling_cars(self): #TODO
-        return
+    def update_travelling_cars(self, current_time):
+        for i in range(len(self.travelling_cars)):
+            (car, start_time) = self.travelling_cars[i]
+            if self.travel_time + start_time == current_time:
+                new_car = self.travelling_cars.pop(i)
+                self.waiting_cars.append(new_car)
 
     def get_waiting_car(self):
-        return self.waiting_cars.pop()
+        return self.waiting_cars.pop(0)
