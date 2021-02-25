576048b6c46b8c3f2adc6eb367332199c2f6f1b6
Shrimat Kapoor <shrimatkapoor@gmail.com>
2021-02-25 19:58:46 +0000

    Modify output file

--- outputfile.py
+++ outputfile.py
@@ -3,9 +3,7 @@ intersections = []
 f.write(str(len(intersections)) + "\n")
 
 for intersection in intersections:
-    f.write(intersection.intersection_id)
-    for street in intersection.incoming_streets:
-        f.write(street.name)
-        # TODO: Add the time schedule for that intersection
+    f.write(intersection.get_schedule())
+    f.write("\n")
 
 f.close()
