6aedc57efc60de814456491a81215667af93c112
Yash Yeola <yash@yashs-mbp.home>
2021-02-25 19:05:20 +0000

    Add queues

--- Street.py
+++ Street.py
@@ -2,6 +2,14 @@ class Street:
 
     def __init__(self, name, begin_node, end_node, travel_time):
         self.name = name
-        self.begin_node = begin_node
-        self.end_node = end_node
+        self.begin_intersection = begin_node
+        self.end_intersection = end_node
         self.travel_time = travel_time
+        self.waiting_cars = []
+        self.travelling_cars = [] #list of tuples (car, start time)
+
+    def update_travelling_cars(self): #TODO
+        return
+
+    def get_waiting_car(self):
+        return self.waiting_cars.pop()
