e45d65d19561ce3d50382829a97a968ecb20bb15
Shrimat Kapoor <shrimatkapoor@gmail.com>
2021-02-25 18:49:43 +0000

    Adds class for streets

---
+++ Street.py
@@ -0,0 +1,7 @@
+class Street:
+
+    def __init__(self, name, begin_node, end_node, travel_time):
+        self.name = name
+        self.begin_node = begin_node
+        self.end_node = end_node
+        self.travel_time = travel_time
