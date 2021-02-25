faca8c4e597bde9a0c52e076d5d1f15f77f7715b
Yash Yeola <yash@yashs-mbp.home>
2021-02-25 19:15:20 +0000

    Add behaviour

--- Car.py
+++ Car.py
@@ -2,3 +2,9 @@ class Car:
     def __init__(self, streets, path):
         self.streets = streets
         self.path = path
+
+    def get_next_road(self):
+        return self.streets.pop(0)
+
+    def finished(self) -> bool:
+        return self.streets.empty()
